7d9feb87de9921bdbc800645a2537092a993cf22
Chris Schneider <chris@christopher-schneider.com>
2012-02-11 10:21:39 -0700

    Initial Commit

---
+++ omnifocus/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  omnifocus
+//
+//  Created by Christopher Schneider on 2/11/12.
+//  Copyright 2012 Personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
